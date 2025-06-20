0fbd3762c3cedf3198f7495420ae5ba3b1e66634
Juanca12082006 <marinonaranjoj@gmail.com>
2025-06-20 10:38:34 -0500

    Update models.py
    Arreglo de los modelos

--- app_euphoria/apl_euphoria/models.py
+++ app_euphoria/apl_euphoria/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 class Administrador(models.Model):
     id_administrador = models.AutoField(primary_key=True)
